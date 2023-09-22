a6192ca1dccbe9563a46a21a0a3de304047db8ac
Đỗ Quang Triệu <116730740+TrieuDoQuang@users.noreply.github.com>
2023-09-22 22:15:10 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      box_shadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+    },
+  },
+  plugins: [],
+}
+
