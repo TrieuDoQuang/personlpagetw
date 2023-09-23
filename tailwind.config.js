b0404697adc9cd8018c9dd806f0f172605c17362
Đỗ Quang Triệu <116730740+TrieuDoQuang@users.noreply.github.com>
2023-09-24 00:02:48 +0700

    complete website

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-
+      box_shadow_cus: "0px 20px 20px 0px rgba(132, 144, 255, 0.3)"
     },
   },
   plugins: [],
